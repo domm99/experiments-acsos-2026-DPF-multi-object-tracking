fa4d5ff31e81c1b72ced2e72a08c3409dcbba685
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-04-22 15:15:45 +0200

    fix: update movingSensorsNB configuration

--- python/plotter/plot_gemini_goes_brrr.py
+++ python/plotter/plot_gemini_goes_brrr.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
     flight_number = 1
 
     # Define the list of experiment names. These must match the subfolder names inside "data/"
-    experiments = ['fixedSensorsNB', 'fixedSensorsLB']
+    experiments = ['fixedSensorsNB', 'fixedSensorsLB', 'movingSensorsNB']
 
     base_charts_path = 'charts'
     base_data_path = 'data'
