b145b6a340cabdb281f9a747e47841134e46f872
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-04-27 22:04:16 +0200

    refactor: centralize particle filter construction

--- src/main/kotlin/it/unibo/collektive/ParticleInitialization.kt
+++ src/main/kotlin/it/unibo/collektive/ParticleInitialization.kt
@@ -1,6 +1,43 @@
 package it.unibo.collektive
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
+import it.unibo.filtering.ParticleFilter
+
+/**
+ * Particle filter parameters read from the simulation environment.
+ */
+internal data class FilterConfiguration(
+    val numberOfParticles: Int,
+    val maxInitialSpeed: Double,
+    val sideLength: Int,
+)
+
+internal val CollektiveDevice<*>.filterConfiguration: FilterConfiguration
+    get() = FilterConfiguration(
+        numberOfParticles = this["NumberOfParticles"],
+        maxInitialSpeed = this["MaxInitialSpeed"],
+        sideLength = this["SideLength"],
+    )
+
+internal fun CollektiveDevice<*>.createParticleFilter(
+    configuration: FilterConfiguration,
+    targetsIds: Set<Int>,
+): ParticleFilter {
+    val sideLength = configuration.sideLength.toDouble()
+    val initializationArea = particleInitializationArea(sideLength)
+    return ParticleFilter(
+        numberOfParticles = configuration.numberOfParticles,
+        maxInitialSpeed = configuration.maxInitialSpeed,
+        sideLength = sideLength,
+        initialMinX = initializationArea.minX,
+        initialMaxX = initializationArea.maxX,
+        initialMinY = initializationArea.minY,
+        initialMaxY = initializationArea.maxY,
+        clampParticlesToInitializationArea = initializationArea.clampParticles,
+        targetsIDs = targetsIds,
+        random = randomGenerator,
+    )
+}
 
 internal fun CollektiveDevice<*>.particleInitializationArea(sideLength: Double): ParticleInitializationArea {
     val halfSideLength = sideLength / 2.0
